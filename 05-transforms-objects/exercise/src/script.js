1e2e9500a685a9002517a415f81d07d9f1d419f1
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-02-14 02:44:14 +0700

    [05] do exercise: scale objects

--- 05-transforms-objects/exercise/src/script.js
+++ 05-transforms-objects/exercise/src/script.js
@@ -19,10 +19,19 @@ scene.add(axesHelper)
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+
+// Position
 mesh.position.x = 0.7
 mesh.position.y = -0.6
 mesh.position.z = 1
 // or `mesh.position.set(0.7, -0.6, 1)`
+
+// Scale
+mesh.scale.x = 2
+mesh.scale.y = 0.25
+mesh.scale.z = 0.5
+// or `mesh.scale.set(2, 0.25, 0.5)`
+
 scene.add(mesh)
 
 /**
