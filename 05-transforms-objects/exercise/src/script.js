5d07089f2aa8c446692613086d3f998408a4856a
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-02-14 02:39:32 +0700

    [05] do exercise: axes helper

--- 05-transforms-objects/exercise/src/script.js
+++ 05-transforms-objects/exercise/src/script.js
@@ -8,6 +8,12 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
+ * Axes helper
+ */
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
+/**
  * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
