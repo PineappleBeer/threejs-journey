0b6a97346e066199222e132930c262f84db0d87b
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-02-14 03:15:17 +0700

    [05] do exercise: scene graph

--- 05-transforms-objects/exercise/src/script.js
+++ 05-transforms-objects/exercise/src/script.js
@@ -40,6 +40,34 @@ mesh.rotation.y = Math.PI * 0.25
 
 scene.add(mesh)
 
+// -----------------------------------------------------------------------------------
+
+// Group
+const group = new THREE.Group()
+group.scale.y = 2
+group.rotation.y = 0.2
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube1.position.x = -1.5
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube3.position.x = 1.5
+group.add(cube3)
+
 /**
  * Sizes
  */
