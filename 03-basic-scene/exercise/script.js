e9f66fa27d5fe9baa4e188344cda6a55891dd0c7
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-02-14 01:45:09 +0700

    [03] do exercise

---
+++ 03-basic-scene/exercise/script.js
@@ -0,0 +1,24 @@
+// Canvas
+const canvas = document.getElementById('webgl')
+
+// Sizes
+const sizes = { width: 800, height: 600 }
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1) // width, height, depth
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height) // fov, aspect
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas })
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
