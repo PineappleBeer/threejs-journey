5fb6db3369d7399d0ab30c30c836aa2f5389b1a6
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-02-23 00:34:48 +0700

    [08] do exercise: handle resize

--- 08-fullscreen-and-resizing/exercise/src/script.js
+++ 08-fullscreen-and-resizing/exercise/src/script.js
@@ -27,6 +27,20 @@ const sizes = {
   height: window.innerHeight,
 }
 
+// handle resize
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+})
+
 /**
  * Camera
  */
