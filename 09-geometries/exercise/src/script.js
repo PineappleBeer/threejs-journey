63f7489c8609e11c8f48245ab8870e2cb4fea729
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-02-25 03:59:59 +0700

    [09] do exercise: BoxGeometry

--- 09-geometries/exercise/src/script.js
+++ 09-geometries/exercise/src/script.js
@@ -12,8 +12,11 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
