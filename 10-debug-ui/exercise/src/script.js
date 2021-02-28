99f78fe6b0cb1099d3faa28b2601cf6ed3083262
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-03-01 03:24:10 +0700

    [10] do exercise

--- 10-debug-ui/exercise/src/script.js
+++ 10-debug-ui/exercise/src/script.js
@@ -2,6 +2,18 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'dat.gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI({ width: 400 }) // press `H` to hide the panel
+const parameters = {
+  color: 0xff0000,
+  spin: () => {
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 })
+  },
+}
 
 /**
  * Base
@@ -16,9 +28,18 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+// Debug
+gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('elevation') // or `gui.add(mesh.position, 'y', -3, 3, 0.01).name('elevation')`
+gui.add(mesh, 'visible')
+gui.add(material, 'wireframe')
+gui.addColor(parameters, 'color').onChange(() => {
+  material.color.set(parameters.color)
+})
+gui.add(parameters, 'spin')
 
 /**
  * Sizes
